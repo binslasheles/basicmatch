6e5701c93e7702a85dc2576798da29e1769d87cf
lee <ls@lsl2-4.local>
2015-02-07 17:18:44 -0600

    serialize test files

---
+++ test/test_serializer.cpp
@@ -0,0 +1,26 @@
+#include "../types.h"
+#include "../serializer.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <assert.h>
+
+
+int main()
+{
+    Serializer s;
+
+    std::string line;
+    std::ifstream ff("invalid.txt", std::ios::in);
+
+    order_action_t a;
+    while (std::getline(ff, line))
+        assert(!s.deserialize(line, a) && a.type_ == action_type_t::ERR);  //std::cout << line << std::endl;
+
+    ff.close();
+    ff.clear();
+    ff.open("valid.txt");
+
+    while (std::getline(ff, line))
+        assert(s.deserialize(line, a) && a.type_ != action_type_t::ERR);  //std::cout << line << std::endl;
+}
